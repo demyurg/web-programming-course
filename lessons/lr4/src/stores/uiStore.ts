e73ca2571fb75722543e0e5dbb594a43d5c36d92
Yartsev Oleg <yartsev.oleg@mail.ru>
2025-11-03 13:04:06 +0700

    Сделан файл uiStore и таск 3

--- lessons/lr4/src/stores/uiStore.ts
+++ lessons/lr4/src/stores/uiStore.ts
@@ -12,10 +12,17 @@ interface UIStore {
   // Состояние
   theme: Theme;
   // TODO: Добавьте другие UI-состояния (soundEnabled)
+  soundEnabled: boolean;
+  sidebarOpen: boolean;
 
   // Actions
   setTheme: (theme: Theme) => void;
   // TODO: Добавьте другие actions (toggleTheme, toggleSound)
+  toggleTheme: () => void;
+  toggleSound: () => void;
+  setSoundEnabled: (enabled: boolean) => void;
+  toggleSidebar: () => void;
+  setSidebarOpen: (open: boolean) => void;
 }
 
 // TODO: Создайте store с помощью create<UIStore>()
@@ -25,15 +32,28 @@ export const useUIStore = create<UIStore>()(
     (set) => ({
       // Начальное состояние
       theme: 'light', // TODO: Добавьте другие поля (soundEnabled и т.д.)
+      soundEnabled: true,
+      sidebarOpen: false,
 
       // Actions
       setTheme: (theme: Theme) => set({ theme }),
 
       // TODO: Добавьте другие actions
-      //   toggleTheme: () => set((state) => ...,
-      //
-      // Пример toggleSound:
-      //   toggleSound: () => set((state) => ({ soundEnabled: !state.soundEnabled })),
+      toggleTheme: () => set((state) => ({ 
+        theme: state.theme === 'light' ? 'dark' : 'light' 
+      })),
+      
+      toggleSound: () => set((state) => ({ 
+        soundEnabled: !state.soundEnabled 
+      })),
+      
+      setSoundEnabled: (enabled: boolean) => set({ soundEnabled: enabled }),
+      
+      toggleSidebar: () => set((state) => ({ 
+        sidebarOpen: !state.sidebarOpen 
+      })),
+      
+      setSidebarOpen: (open: boolean) => set({ sidebarOpen: open }),
     }),
     {
       name: 'ui-storage', // ключ в localStorage
