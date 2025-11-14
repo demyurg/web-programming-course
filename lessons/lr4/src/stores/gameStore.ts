42a56ed040343f99083adc4cde544857ae027643
Loskutova Vika <loskutovave@mail.ru>
2025-11-14 22:39:16 +0500

    Сделана лабораторная 4. Лоскутова

--- lessons/lr4/src/stores/gameStore.ts
+++ lessons/lr4/src/stores/gameStore.ts
@@ -12,11 +12,11 @@ class GameStore {
   gameStatus: 'idle' | 'playing' | 'finished' = 'idle';
 
   // TODO: Добавьте другие поля состояния:
-  // questions: Question[] = [];
-  // currentQuestionIndex = 0;
-  // score = 0;
-  // selectedAnswer: number | null = null;
-  // answeredQuestions: Answer[] = [];
+  questions: Question[] = [];
+  currentQuestionIndex = 0;
+  score = 0;
+  selectedAnswer: number | null = null;
+  answeredQuestions: Answer[] = [];
 
   constructor() {
     makeAutoObservable(this);
@@ -29,6 +29,10 @@ class GameStore {
     // TODO: Добавьте остальную логику:
     // - Загрузите вопросы из mockQuestions
     // - Сбросьте счётчики и индексы
+    this.questions = mockQuestions;
+    this.currentQuestionIndex=0;
+    this.score=0;
+    this.answeredQuestions=[]
   }
 
   selectAnswer(answerIndex: number) {
@@ -39,28 +43,68 @@ class GameStore {
     // 3. Проверьте правильность (сравните с correctAnswer)
     // 4. Увеличьте счёт если правильно
     // 5. Сохраните в историю ответов
+     if (this.selectedAnswer !== null || this.gameStatus !== 'playing'){
+      return
+    }
+    this.selectedAnswer = answerIndex;
+
+    const currentQuestion = this.questions[this.currentQuestionIndex];
+
+    if (answerIndex === currentQuestion.correctAnswer) {
+      this.score++;
+    }
+
+    this.answeredQuestions.push({
+      questionId: currentQuestion.id,
+      selectedAnswer: answerIndex,
+      isCorrect: answerIndex === currentQuestion.correctAnswer,
+      });
   }
 
   // TODO: Добавьте другие методы:
   // nextQuestion() - переход к следующему вопросу
+  nextQuestion() {
+    if (this.isLastQuestion) {
+      this.finishGame();
+    } else {
+      this.currentQuestionIndex++;
+      this.selectedAnswer = null; // сбрасываем выбранный ответ
+    }
+  }
   // finishGame() - завершение игры
+  finishGame() {
+    this.gameStatus = 'finished';
+  }
   // resetGame() - сброс к начальным значениям
-
+  resetGame() {
+      this.gameStatus = 'idle';
+      this.currentQuestionIndex = 0;
+      this.score = 0;
+      this.selectedAnswer = null;
+      this.answeredQuestions = [];
+    }
   // Computed values - вычисляемые значения
 
   get currentQuestion(): Question | null {
     // TODO: Верните текущий вопрос из массива questions
-    return null;
+    return this.questions[this.currentQuestionIndex];;
   }
 
   get progress(): number {
     // TODO: Вычислите прогресс в процентах (0-100)
-    return 0;
+    if (this.questions.length === 0) return 0;
+    return ((this.currentQuestionIndex + 1) / this.questions.length) * 100;
   }
 
   // TODO: Добавьте другие computed values:
   // get isLastQuestion(): boolean
+  get isLastQuestion(): boolean {
+    return this.currentQuestionIndex === this.questions.length - 1;
+  }
   // get correctAnswersCount(): number
+  get correctAnswersCount(): number {
+    return this.answeredQuestions.filter((a) => a.isCorrect).length;
+  }
 }
 
 export const gameStore = new GameStore();
