12af2dc0998d731bb9e89b80fbe1808ef4a43adc
Antonov Dmitry <dmitry_antonov02@mail.ru>
2025-11-28 16:59:05 +0700

    lr3

--- lessons/lr3/src/tasks/Task1.tsx
+++ lessons/lr3/src/tasks/Task1.tsx
@@ -20,13 +20,13 @@ const products: Product[] = [
     id: 1,
     name: 'Наушники',
     price: 5990,
-    image: 'https://images.unsplash.com/photo-1505740420928-5e560c06d30e?w=400&h=300&fit=crop'
+    image: 'https://ir.ozone.ru/s3/multimedia-1-2/7728884354.jpg'
   },
   {
     id: 2,
     name: 'Смарт-часы',
     price: 12990,
-    image: 'https://images.unsplash.com/photo-1523275335684-37898b6baf30?w=400&h=300&fit=crop'
+    image: 'https://img.mvideo.ru/Big/30063244bb.jpg'
   }
 ];
 
@@ -41,15 +41,15 @@ function Task1() {
 
       <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
         {products.map(product => (
-          <div key={product.id}>
+          <div key={product.id} className="bg-white shadow-md rounded-lg p-4 hover:shadow-xl transition-shadow">
             {/* TODO: bg-white shadow-md rounded-lg p-4 hover:shadow-xl transition-shadow */}
             <div>
               {/* TODO: rounded w-full h-40 object-cover */}
-              <img src={product.image} alt={product.name} />
+              <img src={product.image} alt={product.name} className="rounded w-full h-40 object-cover"/>
               {/* TODO: text-lg font-bold mt-3 */}
-              <h3>{product.name}</h3>
+              <h3 className="text-lg font-bold mt-3">{product.name}</h3>
               {/* TODO: text-xl font-bold text-blue-600 mt-2 */}
-              <p>{product.price.toLocaleString('ru-RU')} ₽</p>
+              <p className="text-xl font-bold text-blue-600 mt-2">{product.price.toLocaleString('ru-RU')} ₽</p>
             </div>
           </div>
         ))}
