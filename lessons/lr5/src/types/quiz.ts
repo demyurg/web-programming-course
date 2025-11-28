b81103025a92106b794813bc8bbce2514d3db1b6
Виктория Мироненко <vinenkooo@MacBook-Air-Viktoria.local>
2025-11-29 01:19:41 +0700

    LR5

---
+++ lessons/lr5/src/types/quiz.ts
@@ -0,0 +1,16 @@
+export interface Question {
+  id: string | number;
+  question: string;
+  options: string[];
+  correctAnswer: number;
+  difficulty: 'easy' | 'medium' | 'hard';
+}
+export interface Answer {
+  questionId: string | number;
+  selectedAnswers: number[];
+  isCorrect: boolean;
+}
+
+export type GameStatus = 'idle' | 'playing' | 'paused' | 'finished';
+
+export type Theme = 'light' | 'dark';
