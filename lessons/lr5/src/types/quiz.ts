92a1df75d7aca264252504a534959366c1b8879e
Abramov <new7kryt@gmail.com>
2025-12-01 12:37:20 +0700

    idk

---
+++ lessons/lr5/src/types/quiz.ts
@@ -0,0 +1,17 @@
+export interface Question {
+  id: string;
+  question: string;
+  options: string[];
+  correctAnswer: number;
+  difficulty: 'easy' | 'medium' | 'hard';
+}
+
+export interface Answer {
+  questionId: string;
+  selectedAnswers: number[];
+  isCorrect: boolean;
+}
+
+export type GameStatus = 'idle' | 'playing' | 'paused' | 'finished';
+
+export type Theme = 'light' | 'dark';
