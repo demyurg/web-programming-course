b3772dea8641dfed6981e69eeef5cdcd4832f76e
Polina <poli806@mail.ru>
2026-02-19 02:16:52 +0700

    Выполнен Checkpoint 3

--- lessons/lr5/quiz-backend/src/index.ts
+++ lessons/lr5/quiz-backend/src/index.ts
@@ -1,5 +1,7 @@
+import 'dotenv/config'
 import { serve } from '@hono/node-server'
 import { Hono } from 'hono'
+import auth from './routes/auth.js'
 
 const app = new Hono()
 
@@ -10,6 +12,8 @@ app.get('/', (c) => {
 app.get('/health', async (c) => {
   return c.json({ status: 'ok' })
 });
+
+app.route('/api/auth', auth)
 
 serve({
   fetch: app.fetch,
