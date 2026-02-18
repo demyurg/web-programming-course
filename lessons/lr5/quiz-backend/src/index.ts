a5a36ab6db1a6bcb5aae516750e3107a76e59a56
German Alina <g.alina04@mail.ru>
2026-02-18 22:53:30 +0700

    Checkpoint 3: GitHub OAuth Callback

--- lessons/lr5/quiz-backend/src/index.ts
+++ lessons/lr5/quiz-backend/src/index.ts
@@ -1,11 +1,23 @@
 import { serve } from '@hono/node-server'
 import { Hono } from 'hono'
+import { cors } from 'hono/cors';
+import { logger } from 'hono/logger';
+import auth from './routes/auth.js';
 
 const app = new Hono()
+
+app.use('*', logger());
+app.use('*', cors());
 
 app.get('/', (c) => {
   return c.text('Hello Hono!')
 })
+
+app.get('/health', async (c) => {
+  return c.json({"status": "ok"})
+})
+
+app.route('/api/auth', auth)
 
 serve({
   fetch: app.fetch,
