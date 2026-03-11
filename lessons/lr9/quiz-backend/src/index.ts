c5b9320e0ed4b8d6afdd39a3bf73d389ef8d1077
Лиза Романькова <rejzumatotoro@gmail.com>
2026-03-12 00:59:30 +0700

    Лабораторная работа №9

---
+++ lessons/lr9/quiz-backend/src/index.ts
@@ -0,0 +1,25 @@
+import  authRoute  from './routes/auth.js'
+import { serve } from '@hono/node-server'
+import { Hono } from 'hono'
+import sessionsRoute from './routes/sessions.js'
+import adminRoute from './routes/admin.js'
+
+const app = new Hono()
+
+app.route('/api/sessions', sessionsRoute)
+
+app.route('/api/admin', adminRoute)
+
+app.get('/health', (c) => {
+  return c.json({ status: 'ok'})
+})
+
+app.route('/api/auth', authRoute)
+
+serve({
+  fetch: app.fetch,
+  port: 3000
+}, (info) => {
+  console.log(`Server is running on http://localhost:${info.port}`)
+})
+
